af3794f5edbbbc0572d03430afded654ee5cd186
Luiz Felipe de Lima Silva Tavares da Costa <luizfelipettavares@gmail.com>
2026-03-17 22:25:00 -0300

    Atividade 01

---
+++ Atividade 01 - Introducao ao HTML e CSS/scripts.js
@@ -0,0 +1,15 @@
+function redirecionar() {
+    window.location.href = "mini-blog-tecnologia.html";
+}
+
+function redirecionar2() {
+    window.location.href = "mini-blog-tecnologia2.html";
+}
+
+function redirecionar3() {
+    window.location.href = "mini-blog-tecnologia3.html";
+}
+
+function voltar() {
+    window.location.href = "index.html";
+}
